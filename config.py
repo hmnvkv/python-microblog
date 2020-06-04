d9a579f3edc60e0e373fd59de0adbb29d9ab2f92
hmnvkv <manrique.hamed1610@gmail.com>
2020-06-03 22:40:20 -0500

    Error handling

--- config.py
+++ config.py
@@ -2,7 +2,16 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
+    # database configs
     SECRET_KEY = os.environ.get('SECRE_KEY') or 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    #Mail error handling configs
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWOD')
+    ADMINS = ['manriquehamed999@gmail.com']
